c8ea6c7851e352b584b80f7101a6ef42080cf9b5
Neil Breeden III <neil@htmlbuffet.com>
2016-05-12 21:22:44 -0700

    Adding template for typescript based projects.

---
+++ templates/typescript/src/tests.ts
@@ -0,0 +1,9 @@
+/// <reference path='../typings/main.d.ts' />
+let chai = require('chai');
+let assert = chai.assert;
+
+describe('main', () => {
+    it('template test', () => {
+        assert.equal(1, 1);
+    });
+});
